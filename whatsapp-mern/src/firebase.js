901072df0c487fd06afea38b658203fcd27fdf42
jzou <jzou@vt.edu>
2021-07-30 15:00:32 -0400

    another attempt to add whatsapp-mern folder

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdrgzl5hGAKnSOu_XCuvykQR2OMCvmVfo",
+    authDomain: "whatsapp-mern-ed4ba.firebaseapp.com",
+    projectId: "whatsapp-mern-ed4ba",
+    storageBucket: "whatsapp-mern-ed4ba.appspot.com",
+    messagingSenderId: "184293479781",
+    appId: "1:184293479781:web:c4eb04963807a7ce523d8c"
+  };
