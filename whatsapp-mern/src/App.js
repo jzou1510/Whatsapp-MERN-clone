e63fbcb8bc1339d0115162ed0664372de1ca765c
jzou <jzou@vt.edu>
2021-08-04 18:41:29 -0400

    initial project done. Has realtime pushing capabilities

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,10 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import Pusher from 'pusher-js';
+import axios from './axios';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync')
+      .then(response => {
+        console.log(response.data);
+        setMessages(response.data);
+      });
+  }, []);
   
   useEffect(() => {
     const pusher = new Pusher('b4e76d9da3808453475b', {
@@ -12,17 +22,24 @@ function App() {
     });
 
     const channel = pusher.subscribe('messages'); //the trigger
-    channel.bind('inserted', (data) => {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage]);
     });
-  }, []);
 
+    //cleanup function
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]); //the [messages] is a dependency
+
+  console.log(messages);
 
   return (
     <div className="app">
       <div className="app__body">
       <Sidebar />
-      <Chat />
+      <Chat messages={messages}/>
       </div>
     </div>
   );
--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined,} from "@material-ui/icons";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
+import axios from './axios';
 
 
-function Chat() {
+function Chat({messages}) {
+
+    const [input, setInput] = useState("");
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+
+        await axios.post('/messages/new', {
+            "message": input,
+            "name": "demo app",
+            "timestamp": "Just now",
+            "received": false,
+        });
+
+        setInput('');
+    };
+
     return (
         <div className="chat">
             <div className="chat__header">
@@ -31,29 +48,29 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                <p className="chat__message"> 
-                    <span className="chat__name"> Joy :) </span>
-                    This is a message
-                    <span className="chat__timestamp"> {new Date().toUTCString()} </span>
-                    
-                </p>
+                {messages.map(message => (
+                    <p className={`chat__message ${message.received && 'chat__receiver'}`}> 
+                        <span className="chat__name">{message.name}) </span>
+                        {message.message}
+                        <span className="chat__timestamp"> {message.timestamp} </span>
+                        
+                    </p>
 
-                <p className="chat__message chat__receiver"> 
-                    <span className="chat__name"> Joy :) </span>
-                    This is a message
-                    <span className="chat__timestamp"> {new Date().toUTCString()} </span>
-                    
-                </p>
+                ))}
+
             </div>
 
             <div className="chat__footer">
                 <InsertEmoticonIcon />
                 <form>
-                    <input placeholder="Type a message"
+                    <input 
+                        value={input} 
+                        onChange = {e => setInput(e.target.value)} 
+                        placeholder="Type a message"
                         type="text"
                     />
 
-                    <button type="submit">
+                    <button onClick={sendMessage} type="submit">
                         Send a message
                     </button>
                 </form>
